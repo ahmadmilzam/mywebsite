aafcbeab559d3820542fe62ecb49e6a0d8c942c3
Ahmad Milzam <ahmadmilzam@yahoo.com>
2014-11-25 11:53:49 +0700

    twitter card

--- partials/header.php
+++ partials/header.php
@@ -1,24 +1,31 @@
 <!doctype html>
 <html class="no-js" lang="en">
-  <head>
-    <link rel="author" href="https://plus.google.com/116499314183216878484">
-    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+<head>
+  <link rel="author" href="https://plus.google.com/116499314183216878484">
+  <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
 
-    <link rel="stylesheet" href="assets/css/app<?php echo ENVIRONMENT == 'development' ? '.css' : '.min.css' ?>" />
-    <!-- Hello there! . If your looking at this. Maybe you are the people i'd love to work with. -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/app<?php echo ENVIRONMENT == 'development' ? '.css' : '.min.css' ?>" />
+  <!-- Hello there! . If your looking at this. Maybe you are the people i'd love to work with. -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <meta name="author" content="Ahmad Milzam">
-    <meta name="description" content="Online Portfolio of Ahmad Milzam, a Front-end Web Architect and Developer living in Jakarta, Indonesia">
+  <meta name="author" content="Ahmad Milzam">
+  <meta name="description" content="Online Portfolio of Ahmad Milzam, a Front-end Web Architect and Developer living in Jakarta, Indonesia">
 
-    <meta property="og:title" content="Online Portfolio of Ahmad Milzam">
-    <meta property="og:url" content="http://ahmadmilzam.com">
-    <meta property="og:image" content="http://ahmadmilzam.com/assets/img/cover.jpg">
-    <meta property="og:description" content="Online Portfolio of Ahmad Milzam, a Front-end Web Architect and Developer living in Jakarta, Indonesia">
-    <meta property="og:type" content="website">
-    <meta property="og:site_name" content="Ahmad Milzam - Front-end Web Architect and Developer | Jakarta Indonesia">
+  <meta property="og:title" content="Online Portfolio of Ahmad Milzam">
+  <meta property="og:url" content="http://ahmadmilzam.com">
+  <meta property="og:image" content="http://ahmadmilzam.com/assets/img/cover.jpg">
+  <meta property="og:description" content="Ahmad Milzam, a Front-end Web Architect and Developer living in Jakarta, Indonesia. Specialise in writing, architecting and scaling CSS for big websites in big teams. With a strong focus on performance.">
+  <meta property="og:type" content="website">
+  <meta property="og:site_name" content="Ahmad Milzam - Front-end Web Architect and Developer | Jakarta Indonesia">
 
-    <title>Ahmad Milzam - Front-end Web Architect and Developer | Jakarta Indonesia</title>
-  </head>
-  <body>
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" content="@milzaaam">
+  <meta name="twitter:creator" content="@milzaaam">
+  <meta name="twitter:title" content="Online Portfolio of Ahmad Milzam">
+  <meta name="twitter:description" content="Ahmad Milzam, a Front-end Web Architect and Developer living in Jakarta, Indonesia. Specialise in writing, architecting and scaling CSS for big websites in big teams. With a strong focus on performance.">
+  <meta name="twitter:image:src" content="http://ahmadmilzam.com/assets/img/cover.jpg">
+
+  <title>Ahmad Milzam - Front-end Web Architect and Developer | Jakarta Indonesia</title>
+</head>
+<body>
