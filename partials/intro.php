df911f6fcf5c3d42739065fd4a3f1f3d9cb2e7d3
Ahmad Milzam <ahmadmilzam91@gmail.com>
2015-08-06 03:09:06 +0700

    finalize new layout

--- partials/intro.php
+++ partials/intro.php
@@ -1,8 +1,8 @@
-<div class="layout-header" id="home" itemscope itemtype="http://schema.org/Person">
+<div class="layout-header" id="home">
   <div class="layout-header__intro">
     <div class="layout">
       <div class="layout__unit small-1 medium-3-5">
-        <h1 class="layout-header__headline"><span class="text-bold">Hello, I'm <span itemprop="name">Ahmad Milzam</span>.</span><br><small><span itemprop="brand">Frontend Web Architect &amp; Developer</span><br>from Jakarta, Indonesia.</small></h1>
+        <h1 class="layout-header__headline"><span class="small-block text-bold">Hello,</span> <span class="text-bold">I'm Ahmad Milzam</span>.<br><small>Frontend Web Architect &amp; Developer<br>from Jakarta, Indonesia.</small></h1>
         <p class="lede">
           I'm currently considering new freelance or direct client projects for Q3 and Q4 2015. If you are interested in working together, contact details can be found below.
         </p>
