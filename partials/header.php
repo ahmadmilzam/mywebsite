ff44feec0a7fdc256ddd04fde72677eec6ae2058
Ahmad Milzam <ahmadmilzam91@gmail.com>
2015-08-28 00:58:23 +0700

    change title tag

--- partials/header.php
+++ partials/header.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <title>Frontend Web Architect and Web Developer | Jakarta Indonesia</title>
+  <title>Jakarta Indonesia: Freelance Frontend Web Architect and Web Developer</title>
 
   <link rel="stylesheet" href="assets/css/app<?php echo ENVIRONMENT == 'development' ? '.css' : '.min.css' ?>" />
   <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
